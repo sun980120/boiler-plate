6bb7459a63cd81bf6683bd37143a70d3a6ed7e34
sun980120 <70737532+sun980120@users.noreply.github.com>
2021-07-06 16:41:37 +0900

    second

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,9 +7,6 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
-import VideoUploadPage from "./views/VideoUploadPage/VideoUploadPage";
-import VideoDetailPage from "./views/VideoDetailPage/VideoDetailPage";
-import SubscriptionPage from './views/SubscriptionPage/SubscriptionPage';
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
